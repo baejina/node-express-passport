618e59626d77bca328fc387a94e3868d37a92a9b
baeg <wlsk1103@gmail.com>
2017-12-05 19:00:46 +0900

    Remove command

--- config/mysql/express.js
+++ config/mysql/express.js
@@ -4,23 +4,17 @@ module.exports = function() {
     var MYSQLStore = require('express-mysql-session')(session);
     var bodyParser = require('body-parser');
 
-    //------------------
-    // :: 4. db 접속코드를 분리함. passport 설정에 쿼리 날리는 부분때문에 에러남.
-    //       해결방법은 주입하거나, 분리해서 require하면 되는데 이경우는 분리함. 해당파일에선 connect를 리턴해
-    //------------------
     var db = require('../../config/mysql/db');
     var app = express();
 
     //------------------
-    // :: 1. jade 사용하려면 추가해줘야됨
-    //    템플릿엔진을 jade로 하겠다고 설정.
+    // :: 1. j템플릿엔진을 jade로 하겠다고 설정.
     //------------------
     app.set('views', './views/mysql');
     app.set('view engine', 'jade');
     //------------------
 
     app.use(bodyParser.urlencoded({ extended: false }));
-
     app.use(session({
         secret: '1234DFs@asd1234!@#$asd',
         resave: false,
@@ -33,6 +27,5 @@ module.exports = function() {
             database: 'o2'
         } )
     }));
-
     return app;
 };
